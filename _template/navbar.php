bfa9f010bd6c02c1ba8b7dd5f56f0a9cb925d40f
ak <arunkumar132004@gmail.com>
2026-04-05 06:43:58 +0000

    navbar position changes

--- _template/navbar.php
+++ _template/navbar.php
@@ -17,8 +17,8 @@ $socials = $p['socials'] ?? [];
         <li><a href="#about"      class="nav-link">About</a></li>
         <li><a href="#skills"     class="nav-link">Skills</a></li>
         <li><a href="#experience" class="nav-link">Experience</a></li>
-        <li><a href="#education"  class="nav-link">Education</a></li>
         <li><a href="#projects"   class="nav-link">Projects</a></li>
+        <li><a href="#education"  class="nav-link">Education</a></li>
         <li><a href="#contact"    class="nav-link">Contact</a></li>
       </ul>
     </div>
